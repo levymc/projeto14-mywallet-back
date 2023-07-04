af58495dbe9e790c13cea1c8af4ff0f435148818
levymc <levymcruz@gmail.com>
2023-07-04 14:22:04 -0300

    adding participants validation in cadastro endpoint

--- src/app.js
+++ src/app.js
@@ -38,13 +38,20 @@ app.post('/cadastro', async (req, res) => {
     const { error: validationError } = schemaCadastro.validate({ email, senha }); // Renomeando para validationError
   
     if (validationError) { // Usando validationError
-      return res.status(422).send("Erro 422 - Algum dado inválido foi inserido");
+        console.log(validationError)
+        return res.status(422).send("Erro 422 - Algum dado inválido foi inserido");
     }
-    hash.update(senha)
-    senha = hash.digest('hex')
+    try{
+        const participant = await db.collection("cadastro").find({email:{$eq: email}}).toArray()
+        if(participant === []) return res.status(409).send("Erro 409 - email já cadastrado.")
+    }catch(err){
+        console.log(err)
+    }
+    // hash.update(senha)
+    // senha = hash.digest('hex')
     console.log(senha) // Senha criptografada em sha256
-    res.status(200).json({ message: 'Cadastro realizado com sucesso!' });
-  });
+    res.status(201).json({ message: 'Cadastro realizado com sucesso!' });
+});
   
 
 
