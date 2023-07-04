eaa9f7ea8dab0c4621fde99dd06758f556d064fa
levymc <levymcruz@gmail.com>
2023-07-04 11:48:53 -0300

    creating the post cadastro endpoint!

--- src/app.js
+++ src/app.js
@@ -8,6 +8,7 @@ const app = express()
 
 app.use(cors())
 app.use(express.json())
+app.use(express.urlencoded({ extended: true }));
 
 const mongoClient = new MongoClient(process.env.DATABASE_URL);
 let db;
@@ -23,9 +24,15 @@ const run = async () => {
     } catch (err) {
       console.error('Erro ao conectar no banco:', err)
     }
-    db =  mongoClient.db()
+    db =  mongoClient.db('projeto14')
 };
 
+
+app.post('/cadastro', async (req, res) => {
+    const { nome, email, senha, confirmarSenha } = req.body;
+    console.log(nome, email, senha, confirmarSenha)
+    res.status(200).json({ message: 'Cadastro realizado com sucesso!' });
+})
 
 
 
