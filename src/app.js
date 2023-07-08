62df1f3a66aaa985fda7fa2f2e4c9e8c2ffad14d
Levy <levymcruz@gmail.com>
2023-07-08 16:25:31 -0300

    trying all

--- src/app.js
+++ src/app.js
@@ -26,7 +26,7 @@ app1.use((err, req, res, next) => {
 let mode = 'prod';
 
 export const URI = mode === 'dev' ?  process.env.DATABASE_URL_DEV : process.env.DATABASE_URL;
-const mongoClient = new MongoClient("mongodb+srv://"+URI, {useNewUrlParser: true, useUnifiedTopology: true});
+const mongoClient = new MongoClient("mongodb+srv://"+URI);
 export let db;
 
 
