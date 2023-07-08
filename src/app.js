a1ce61f947e785418c92da2ef7635c78f6b95cc6
Levy <levymcruz@gmail.com>
2023-07-08 12:58:57 -0300

    dev -> prod

--- src/app.js
+++ src/app.js
@@ -25,7 +25,7 @@ app.use((err, req, res, next) => {
 });
 
 
-let mode = 'dev';
+let mode = 'prod';
 
 
 export const URI = mode === 'dev' ?  process.env.DATABASE_URL_DEV : process.env.DATABASE_URL;
